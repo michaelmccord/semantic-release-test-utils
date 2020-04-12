d26f50f73c366775726f83c5074166e036810a9b
Michael McCord <michael.mccord@mdbuyline.com>
2020-04-11 21:33:26 -0400

    feat: initial commit of files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # semantic-release-test-utils
+
+Contents of this library were originally [written by the authors of semantic-release](https://github.com/semantic-release/semantic-release/tree/caa3526caa686c18eb935dace80a275017746215/test/helpers). They are exposed here for the convenience of testing semantic-release plugins.
---
+++ index.js
@@ -0,0 +1,13 @@
+const gitUtils = require('./src/git-utils');
+const gitbox = require('./src/gitbox');
+const mockServer = require('./src/mockserver');
+const npmregistry = require('./src/npm-registry');
+const npmutils = require('./src/npm-utils');
+
+module.exports = {
+    gitUtils,
+    gitbox,
+    mockServer,
+    npmregistry,
+    npmutils
+};
